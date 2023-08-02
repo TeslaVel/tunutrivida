50c7c2ccabad5349a7cd66e45b82f841c83c5e7a
TeslaVel <teslaveloper@gmail.com>
2023-08-02 13:29:53 -0400

    adds proper css color

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -25,22 +25,22 @@ Turbolinks.start()
 ActiveStorage.start()
 Chartkick.use(Chart)
 
-datatable(window, $)
-datatableBS4(window, $)
+// datatable(window, $)
+// datatableBS4(window, $)
 
 import '@fortawesome/fontawesome-free/js/all'
-import datatable from 'imports-loader?define=>false!datatables.net'
-import datatableBS4 from 'imports-loader?define=>false!datatables.net-bs4'
+// import datatable from 'imports-loader?define=>false!datatables.net'
+// import datatableBS4 from 'imports-loader?define=>false!datatables.net-bs4'
 import './shared/sb-admin-2'
 import './shared/functions'
 
 
 import './shared/charts'
 
-import './shared/demo/datatables-demo'
-import './shared/demo/chart-area-demo'
-import './shared/demo/chart-bar-demo'
-import './shared/demo/chart-pie-demo'
+// import './shared/demo/datatables-demo'
+// import './shared/demo/chart-area-demo'
+// import './shared/demo/chart-bar-demo'
+// import './shared/demo/chart-pie-demo'
 
 let found_class_date = $('.datetime').val();
 if (found_class_date)
